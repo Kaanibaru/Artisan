fb90aa96afb0a74790d13093cc8c5df6dc7d81b8
Kaanibaru <49502397+Kaanibaru@users.noreply.github.com>
2024-07-30 15:23:56 +0200

    Update DalamudInfo.cs

--- Artisan/RawInformation/DalamudInfo.cs
+++ Artisan/RawInformation/DalamudInfo.cs
@@ -12,7 +12,6 @@ namespace Artisan.RawInformation
         public static bool IsStaging = false;
         public static bool IsOnStaging()
         {
-//#if DEBUG
 //            return false;
 //#endif
             if (StagingChecked)
